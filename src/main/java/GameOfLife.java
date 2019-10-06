6f37d81b346a667fc8cf01db81d9a7e7e1baf7ca
Sush25b <phulsundarsushant26@gmail.com>
2019-10-06 14:59:00 +0530

    [Sushant] Add.CLuster when no codition applied gives Same evolve cluster

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,4 +1,8 @@
 public class GameOfLife
 {
-
+    public static int[] evolvePattern(int[] initialCluster)
+    {
+        int[] evolvePattern={0,0,0,0};
+        return evolvePattern;
+    }
 }
--- src/test/java/GameOFLifeTest.java
+++ src/test/java/GameOFLifeTest.java
@@ -5,5 +5,12 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class GameOFLifeTest
 {
+    @Test
+    public void givenOneClusterWithAllDead_whenApplyNoCondition_thenReturnEvolveClusterOFAllDead()
+    {
+        int[] initialCluster ={0,0,0,0};
+        int[] evolvedCluster ={0,0,0,0};
 
+        assertArrayEquals(evolvedCluster, initialCluster);
+    }
 }
