8a9266c50ed784aad15cfbb58f9582d4e47d6aa2
Sush25b <phulsundarsushant26@gmail.com>
2019-10-07 11:30:01 +0530

    [Sushant] Fix. change Array input to list for better operation

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,27 +1,41 @@
-public class GameOfLife
-{
-    Rule rule;
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-    public GameOfLife(Rule rule)
-    {
+public class GameOfLife {
+    private Rule rule;
+    private List<Integer> evolvedCluster = new ArrayList<>();
+    private List<Integer> paddedCluster= new ArrayList<>();;
+
+    public GameOfLife(Rule rule) {
         this.rule = rule;
     }
 
-    public int[] evolvePattern(int[] initialCluster)
-    {
-        for(int i=0;i<=initialCluster.length;i++)
-        {
-            boolean flag=false;
-            if(initialCluster[i]==rule.current && initialCluster[i-1]==rule.left && initialCluster[i+1]==rule.right)
-            {
-                flag=true;
-            }
-            if(flag)
-            {
-                initialCluster[i]=1;
+    public List<Integer> evolvePattern(List<Integer> initialCluster) {
+        paddedCluster = paddingEvolvedCluster(initialCluster);
+
+        for (int i = 1; i < paddedCluster.size() - 1; i++) {
+            if (paddedCluster.get(i) == rule.current && paddedCluster.get(i - 1) == rule.left && paddedCluster.get(i + 1) == rule.right) {
+                evolvedCluster.add(1);
+            } else {
+                evolvedCluster.add(0);
             }
         }
-        return initialCluster;
+
+        return evolvedCluster;
     }
 
+    private List<Integer> paddingEvolvedCluster(List<Integer> clusterToPad) {
+        int valueAtEnds = 0;
+
+        paddedCluster.add(valueAtEnds);
+        for (Object i : clusterToPad) {
+            paddedCluster.add((Integer) i);
+        }
+        paddedCluster.add(valueAtEnds);
+
+        return paddedCluster;
+    }
 }
--- src/test/java/GameOFLifeTest.java
+++ src/test/java/GameOFLifeTest.java
@@ -1,28 +1,23 @@
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class GameOFLifeTest
-{
+public class GameOFLifeTest {
     @Test
-    public void givenOneClusterWithAllDead_whenApplyNoCondition_thenReturnEvolveClusterOFAllDead()
-    {
-        int[] initialCluster ={0,0,0,0};
-        int[] evolvedCluster ={0,0,0,0};
+    public void givenOneClusterWithAllDead_whenApplyOneCondition_thenReturnEvolveClusterOFAllAlive() {
+        List<Integer> initialCluster = Arrays.asList(0,0,0,0);
+        Rule rule = new Rule(0, 0, 0);
+        GameOfLife gameOfLife = new GameOfLife(rule);
+        List<Integer> evolvedCluster = Arrays.asList(1,1,1,1);
 
-        assertArrayEquals(initialCluster,evolvedCluster);
+        assertEquals(evolvedCluster, gameOfLife.evolvePattern(initialCluster));
     }
 
-    @Test
-    public void givenOneClusterWithAllDead_whenApplyOneCondition_thenReturnEvolveClusterOFAllDead()
-    {
-        int[] initialCluster ={0,0,0,0};
-        Rule rule=new Rule(0,0,0);
-        int[] evolvedCluster ={0,0,0,0};
 
-        assertArrayEquals(initialCluster,evolvedCluster);
-    }
 }
