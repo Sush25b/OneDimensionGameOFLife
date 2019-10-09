5bac274c8c4aae840d4f9b283de06afee00642db
Sush25b <phulsundarsushant26@gmail.com>
2019-10-09 09:31:40 +0530

    [Sushant] Add. capability to Evolve pattern recursively with multiple rules applied

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -3,45 +3,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameOfLife {
-    private List<Rule> rule;
+    private List<Rule> rules;
     private List<Integer> evolvedCluster = new ArrayList<>();
     private List<Integer> paddedCluster = new ArrayList<>();
+    private List<Integer> initialCluster;
 
-    public GameOfLife(List<Rule> rule) {
-        this.rule = rule;
+    public GameOfLife(List<Rule> rules, List<Integer> initialCluster) {
+        this.rules = rules;
+        this.initialCluster = initialCluster;
     }
 
-    public List<Integer> evolvePattern(List<Integer> initialCluster) {
-
-        paddedCluster = paddingEvolvedCluster(initialCluster);
+    private void initializeEvolveCluster(List<Integer> initialCluster) {
         for (int i = 0; i < initialCluster.size(); i++) {
             evolvedCluster.add(0);
         }
+    }
 
-        for (Rule currentRule : rule) {
-            for (int i = 1; i < paddedCluster.size() - 1; i++) {
-               evolvePattern(i,currentRule);
+    public List<Integer> evolvePattern() {
+        paddedCluster = pad(initialCluster);
+        initializeEvolveCluster(initialCluster);
+
+        for (Rule currentRule : rules) {
+            for (int i = 1; i < paddedCluster.size() - 1; i++) {         // TODO - name your loops (private methods)
+                patternMatcher(i, currentRule);
             }
         }
         return evolvedCluster;
     }
 
-    private void evolvePattern(int i,Rule currentRule)
-    {
-        if (paddedCluster.get(i) == currentRule.current && paddedCluster.get(i - 1) == currentRule.left && paddedCluster.get(i + 1) == currentRule.right) {
+    private void patternMatcher(int i, Rule currentRule) {
+         boolean checkCurrent = paddedCluster.get(i) == currentRule.current;
+         boolean checkLeft= paddedCluster.get(i - 1) == currentRule.left;
+         boolean checkRight=paddedCluster.get(i + 1) == currentRule.right;
+
+        if (checkCurrent && checkLeft && checkRight) {
             evolvedCluster.set(i - 1, 1);
         }
     }
 
-    private List<Integer> paddingEvolvedCluster(List<Integer> clusterToPad) {
+    private List<Integer> pad(List<Integer> cluster) {
         int valueAtEnds = 0;
 
         paddedCluster.add(valueAtEnds);
-        for (Object i : clusterToPad) {
-            paddedCluster.add((Integer) i);
-        }
+        paddedCluster.addAll(cluster);
         paddedCluster.add(valueAtEnds);
 
         return paddedCluster;
     }
+
+   public GameOfLife getEvolvedGameOfLife()
+   {
+       return new GameOfLife(this.rules,this.evolvePattern());
+       
+   }
 }
--- src/test/java/GameOFLifeTest.java
+++ src/test/java/GameOFLifeTest.java
@@ -1,12 +1,10 @@
-
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GameOFLifeTest {
     @Test
@@ -14,10 +12,11 @@ public class GameOFLifeTest {
         List<Integer> initialCluster = Arrays.asList(0,0,0,0);
         Rule rule = new Rule(0, 0, 0);
         List<Rule> ruleList= Arrays.asList(rule);
-        GameOfLife gameOfLife = new GameOfLife(ruleList);
+        GameOfLife gameOfLife = new GameOfLife(ruleList,initialCluster);
+
         List<Integer> evolvedCluster = Arrays.asList(1,1,1,1);
 
-        assertEquals(evolvedCluster, gameOfLife.evolvePattern(initialCluster));
+        assertEquals(evolvedCluster, gameOfLife.evolvePattern());
     }
 
     @Test
@@ -25,10 +24,11 @@ public class GameOFLifeTest {
         List<Integer> initialCluster = Arrays.asList(0,0,0,1);
         Rule rule = new Rule(0, 0, 0);
         List<Rule> ruleList= Arrays.asList(rule);
-        GameOfLife gameOfLife = new GameOfLife(ruleList);
+        GameOfLife gameOfLife = new GameOfLife(ruleList,initialCluster);
+
         List<Integer> evolvedCluster = Arrays.asList(1,1,0,0);
 
-        assertEquals(evolvedCluster, gameOfLife.evolvePattern(initialCluster));
+        assertEquals(evolvedCluster, gameOfLife.evolvePattern());
     }
 
     @Test
@@ -38,10 +38,11 @@ public class GameOFLifeTest {
         Rule firstRule = new Rule(0, 1, 0);
         Rule secondRule = new Rule(0, 0, 1);
         List<Rule> ruleList= Arrays.asList(firstRule,secondRule);
-        GameOfLife gameOfLife = new GameOfLife(ruleList);
+        GameOfLife gameOfLife = new GameOfLife(ruleList,initialCluster);
+
         List<Integer> evolvedCluster = Arrays.asList(1,0,1,1);
 
-        assertEquals(evolvedCluster, gameOfLife.evolvePattern(initialCluster));
+        assertEquals(evolvedCluster, gameOfLife.evolvePattern());
     }
 
     @Test
@@ -51,9 +52,24 @@ public class GameOFLifeTest {
         Rule firstRule = new Rule(0, 1, 0);
         Rule secondRule = new Rule(0, 0, 1);
         List<Rule> ruleList= Arrays.asList(firstRule,secondRule);
-        GameOfLife gameOfLife = new GameOfLife(ruleList);
+        GameOfLife gameOfLife = new GameOfLife(ruleList,initialCluster);
+
         List<Integer> evolvedCluster = Arrays.asList(1,0,0,0);
 
-        assertEquals(evolvedCluster, gameOfLife.evolvePattern(initialCluster));
+        assertEquals(evolvedCluster, gameOfLife.evolvePattern());
+    }
+
+
+    @Test
+    public void givenOneClusterWithOnlyTwoAlive_whenApplyTwoConditionAndEvolveTwice_thenReturnEvolveClusterOFOneAlive() {
+        List<Integer> initialCluster = Arrays.asList(0, 1, 1, 0);
+        Rule firstRule = new Rule(0, 1, 0);
+        Rule secondRule = new Rule(0, 0, 1);
+        List<Rule> ruleList = Arrays.asList(firstRule, secondRule);
+        GameOfLife gameOfLife = new GameOfLife(ruleList, initialCluster);
+
+        List<Integer> evolvedCluster = Arrays.asList(1, 0, 0, 0);
+
+        assertEquals(evolvedCluster, gameOfLife.getEvolvedGameOfLife().evolvePattern());
     }
 }
